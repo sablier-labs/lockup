d45e6dcc34e2a83eefa4f7460dd59a7ac3959ba6
andreivladbrg <andreivladbrg@gmail.com>
2023-11-01 12:28:22 +0200

    test: withdrawableAmountOf function

---
+++ test/integration/withdrawable-amount-of /withdrawableAmountOf.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.20;
+
+import { Integration_Test } from "../Integration.t.sol";
+
+contract WithdrawableAmountOf_Integration_Test is Integration_Test {
+    function setUp() public override {
+        Integration_Test.setUp();
+    }
+
+    function test_RevertGiven_Null() external {
+        _test_RevertGiven_Null();
+        openEnded.withdrawableAmountOf(nullStreamId);
+    }
+
+    function test_RevertGiven_Canceled() external givenNotNull {
+        _test_RevertGiven_Canceled();
+        openEnded.withdrawableAmountOf(defaultStreamId);
+    }
+
+    function test_WithdrawableAmountOf_BalanceZero() external givenNotNull givenNotCanceled {
+        uint256 withdrawableAmount = openEnded.withdrawableAmountOf(defaultStreamId);
+        assertEq(withdrawableAmount, 0, "withdrawable amount");
+    }
+
+    function test_WithdrawableAmountOf_BalanceLessThanOrEqualStreamedAmount() external givenNotNull givenNotCanceled {
+        uint128 depositAmount = 1e18;
+        openEnded.deposit(defaultStreamId, depositAmount);
+
+        vm.warp({ newTimestamp: WARP_ONE_MONTH });
+        uint256 withdrawableAmount = openEnded.withdrawableAmountOf(defaultStreamId);
+        assertEq(withdrawableAmount, depositAmount, "withdrawable amount");
+    }
+
+    function test_WithdrawableAmountOf() external givenNotNull givenNotCanceled {
+        defaultDeposit();
+
+        vm.warp({ newTimestamp: WARP_ONE_MONTH });
+        uint256 withdrawableAmount = openEnded.withdrawableAmountOf(defaultStreamId);
+        assertEq(withdrawableAmount, ONE_MONTH_STREAMED_AMOUNT, "withdrawable amount");
+    }
+}
