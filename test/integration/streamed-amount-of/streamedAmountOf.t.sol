ce7549efc00de277a57931a17ecfd2fb215fcca2
andreivladbrg <andreivladbrg@gmail.com>
2023-11-01 12:27:58 +0200

    test: streamedAmountOf function

---
+++ test/integration/streamed-amount-of/streamedAmountOf.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.20;
+
+import { Integration_Test } from "../Integration.t.sol";
+
+contract StreamedAmountOf_Integration_Test is Integration_Test {
+    function setUp() public override {
+        Integration_Test.setUp();
+    }
+
+    function test_RevertGiven_Null() external {
+        _test_RevertGiven_Null();
+        openEnded.streamedAmountOf(nullStreamId);
+    }
+
+    function test_RevertGiven_Canceled() external givenNotNull {
+        _test_RevertGiven_Canceled();
+        openEnded.streamedAmountOf(defaultStreamId);
+    }
+
+    function test_StreamedAmountOf_LastTimeUpdateInThePresent() external givenNotNull givenNotCanceled {
+        uint256 streamedAmount = openEnded.streamedAmountOf(defaultStreamId);
+        assertEq(streamedAmount, 0, "streamed amount");
+    }
+
+    function test_StreamedAmountOf() external givenNotNull givenNotCanceled {
+        vm.warp({ newTimestamp: WARP_ONE_MONTH });
+        uint256 streamedAmount = openEnded.streamedAmountOf(defaultStreamId);
+        assertEq(streamedAmount, ONE_MONTH_STREAMED_AMOUNT, "streamed amount");
+    }
+}
