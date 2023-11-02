2c3f539ce5cfedb1162dc0d68e49b776e82d4cf5
andreivladbrg <andreivladbrg@gmail.com>
2023-11-02 14:57:09 +0200

    test: cancel function

---
+++ test/integration/cancel/cancel.t.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.20;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { ISablierV2OpenEnded } from "src/interfaces/ISablierV2OpenEnded.sol";
+import { Errors } from "src/libraries/Errors.sol";
+import { OpenEnded } from "src/types/DataTypes.sol";
+
+import { Integration_Test } from "../Integration.t.sol";
+
+contract Cancel_Integration_Test is Integration_Test {
+    function setUp() public override {
+        Integration_Test.setUp();
+
+        vm.warp({ newTimestamp: WARP_ONE_MONTH });
+    }
+
+    function test_RevertWhen_DelegateCall() external {
+        bytes memory callData = abi.encodeCall(ISablierV2OpenEnded.cancel, (defaultStreamId));
+        _test_RevertWhen_DelegateCall(callData);
+    }
+
+    function test_RevertGiven_Null() external whenNotDelegateCalled {
+        _test_RevertGiven_Null();
+        openEnded.cancel(nullStreamId);
+    }
+
+    function test_RevertGiven_Canceled() external whenNotDelegateCalled givenNotNull {
+        _test_RevertGiven_Canceled();
+        openEnded.cancel(defaultStreamId);
+    }
+
+    function test_RevertWhen_CallerUnauthorized_Recipient()
+        external
+        whenNotDelegateCalled
+        givenNotNull
+        givenNotCanceled
+        whenCallerUnauthorized
+    {
+        changePrank({ msgSender: users.recipient });
+        vm.expectRevert(
+            abi.encodeWithSelector(Errors.SablierV2OpenEnded_Unauthorized.selector, defaultStreamId, users.recipient)
+        );
+        openEnded.cancel(defaultStreamId);
+    }
+
+    function test_RevertWhen_CallerUnauthorized_MaliciousThirdParty(address maliciousThirdParty)
+        external
+        whenNotDelegateCalled
+        givenNotNull
+        givenNotCanceled
+        whenCallerUnauthorized
+    {
+        vm.assume(maliciousThirdParty != users.sender && maliciousThirdParty != users.recipient);
+        changePrank({ msgSender: maliciousThirdParty });
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Errors.SablierV2OpenEnded_Unauthorized.selector, defaultStreamId, maliciousThirdParty
+            )
+        );
+        openEnded.cancel(defaultStreamId);
+    }
+
+    function test_Cancel_RefundableAmountZero_WithdrawableAmountZero()
+        external
+        whenNotDelegateCalled
+        givenNotNull
+        givenNotCanceled
+        whenCallerAuthorized
+    {
+        openEnded.cancel(defaultStreamId);
+
+        assertTrue(openEnded.isCanceled(defaultStreamId), "is canceled");
+
+        uint128 actualStreamBalance = openEnded.getBalance(defaultStreamId);
+        assertEq(actualStreamBalance, 0, "stream balance");
+
+        uint256 actualAmountPerSecond = openEnded.getAmountPerSecond(defaultStreamId);
+        assertEq(actualAmountPerSecond, 0, "amount per second");
+    }
+
+    function test_Cancel_AssetNot18Decimals()
+        external
+        whenNotDelegateCalled
+        givenNotNull
+        givenNotCanceled
+        whenCallerAuthorized
+        whenRefundAmountNotZero
+        whenNoOverrefund
+    {
+        // Set the timestamp to 1 month ago to create the stream with the same `lastTimeUpdate` as `defaultStreamId`.
+        vm.warp({ newTimestamp: WARP_ONE_MONTH - ONE_MONTH });
+        uint256 streamId = createDefaultStreamWithAsset(IERC20(address(usdt)));
+        openEnded.deposit(streamId, DEPOSIT_AMOUNT);
+        vm.warp({ newTimestamp: WARP_ONE_MONTH });
+
+        test_Cancel(streamId, IERC20(address(usdt)));
+    }
+
+    function test_Cancel()
+        external
+        whenNotDelegateCalled
+        givenNotNull
+        givenNotCanceled
+        whenCallerAuthorized
+        whenRefundAmountNotZero
+        whenNoOverrefund
+    {
+        test_Cancel(defaultStreamId, dai);
+    }
+
+    function test_Cancel(uint256 streamId, IERC20 asset) internal {
+        defaultDeposit();
+
+        uint128 refundableAmount = openEnded.refundableAmountOf(streamId);
+        uint128 withdrawableAmount = openEnded.withdrawableAmountOf(streamId);
+
+        vm.expectEmit({ emitter: address(asset) });
+        emit Transfer({
+            from: address(openEnded),
+            to: users.sender,
+            value: normalizeTransferAmount(streamId, refundableAmount)
+        });
+
+        vm.expectEmit({ emitter: address(asset) });
+        emit Transfer({
+            from: address(openEnded),
+            to: users.recipient,
+            value: normalizeTransferAmount(streamId, withdrawableAmount)
+        });
+
+        vm.expectEmit({ emitter: address(openEnded) });
+        emit CancelOpenEndedStream({
+            streamId: streamId,
+            sender: users.sender,
+            recipient: users.recipient,
+            senderAmount: refundableAmount,
+            recipientAmount: withdrawableAmount,
+            asset: asset
+        });
+
+        expectCallToTransfer({
+            asset: asset,
+            to: users.sender,
+            amount: normalizeTransferAmount(streamId, refundableAmount)
+        });
+        expectCallToTransfer({
+            asset: asset,
+            to: users.recipient,
+            amount: normalizeTransferAmount(streamId, withdrawableAmount)
+        });
+        openEnded.cancel(streamId);
+
+        assertTrue(openEnded.isCanceled(streamId), "is canceled");
+
+        uint128 actualStreamBalance = openEnded.getBalance(streamId);
+        assertEq(actualStreamBalance, 0, "stream balance");
+
+        uint256 actualAmountPerSecond = openEnded.getAmountPerSecond(streamId);
+        assertEq(actualAmountPerSecond, 0, "amount per second");
+    }
+}
