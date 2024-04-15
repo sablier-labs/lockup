59ae9f0cad9ef93da43cf7e0b6a2061b0fc05463
andreivladbrg <andreivladbrg@gmail.com>
2024-04-15 16:25:18 +0200

    feat: add deployment scripts

---
+++ script/DeployOpenEnded.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.22 <0.9.0;
+
+import { SablierV2OpenEnded } from "src/SablierV2OpenEnded.sol";
+
+import { BaseScript } from "./Base.s.sol";
+
+contract DeployOpenEnded is BaseScript {
+    function run() public broadcast returns (SablierV2OpenEnded openEnded) {
+        openEnded = new SablierV2OpenEnded();
+    }
+}
