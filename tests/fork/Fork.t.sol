cb1fa5b26b53d8670be01d29690c6790ebea54d2
smol-ninja <shubhamy2015@gmail.com>
2025-03-02 13:27:43 +0000

    ci: run fork tests on latest block

--- tests/fork/Fork.t.sol
+++ tests/fork/Fork.t.sol
@@ -31,8 +31,8 @@ abstract contract Fork_Test is Base_Test, Merkle {
     //////////////////////////////////////////////////////////////////////////*/
 
     function setUp() public virtual override {
-        // Fork Ethereum Mainnet at a specific block number.
-        vm.createSelectFork({ blockNumber: 21_719_244, urlOrAlias: "mainnet" });
+        // Fork Ethereum Mainnet at the latest block number.
+        vm.createSelectFork({ urlOrAlias: "mainnet" });
 
         // Load deployed addresses from Ethereum mainnet.
         merkleFactory = ISablierMerkleFactory(0x71DD3Ca88E7564416E5C2E350090C12Bf8F6144a);
