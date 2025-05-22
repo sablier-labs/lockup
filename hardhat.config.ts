36f0daee94618e462bdcc70549d92c7c94a8797f
Lutro <76906369+lutr0@users.noreply.github.com>
2025-05-22 22:29:24 +0200

    fix: sophscan verification (#1255)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -68,10 +68,38 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
+    enabled: true,
     apiKey: {
-      sophonMainnet: "",
+      sophonTestnet: process.env.ETHERSCAN_SOPHON_API_KEY as string,
+      sophonMainnet: process.env.ETHERSCAN_SOPHON_API_KEY as string,
     },
-  },
+    customChains: [
+      {
+        network: "sophonTestnet",
+        chainId: 531050104,
+        urls: {
+          apiURL: "https://api-testnet.sophscan.xyz/api",
+          browserURL: "https://testnet.sophscan.xyz",
+        },
+      },
+      {
+        network: "sophonMainnet",
+        chainId: 50104,
+        urls: {
+          apiURL: "https://api.sophscan.xyz/api",
+          browserURL: "https://sophscan.xyz",
+        },
+      },
+      {
+        network: "zkSyncTestnet",
+        chainId: 300,
+        urls: {
+          apiURL: "https://api-testnet-era.zksync.network/api",
+          browserURL: "https://testnet-era.zksync.network",
+        },
+      },
+    ],
+  }
 };
 
 export default config;
