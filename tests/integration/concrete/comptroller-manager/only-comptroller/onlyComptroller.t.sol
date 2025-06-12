5278e40acd3b62044f95bea26983edad0f1a827b
smol-ninja <shubhamy2015@gmail.com>
2025-06-12 14:12:30 +0100

    test: adds tests for onlyComptroller modifier

---
+++ tests/integration/concrete/comptroller-manager/only-comptroller/onlyComptroller.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.22;
+
+import { Errors } from "src/libraries/Errors.sol";
+import { Base_Test } from "../../../../Base.t.sol";
+
+contract OnlyComptroller_ComptrollerManager_Concrete_Test is Base_Test {
+    function test_RevertWhen_CallerNotComptroller() external {
+        // It should revert.
+        vm.expectRevert(
+            abi.encodeWithSelector(Errors.ComptrollerManager_CallerNotComptroller.selector, comptroller, admin)
+        );
+        comptrollerManagerMock.restrictedToComptroller();
+    }
+
+    function test_WhenCallerComptroller() external {
+        setMsgSender(address(comptroller));
+
+        // It should execute the function.
+        comptrollerManagerMock.restrictedToComptroller();
+    }
+}
