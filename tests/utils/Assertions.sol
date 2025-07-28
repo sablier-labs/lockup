8ba8baa961cceb159270c00b535876f387c00f77
smol-ninja <shubhamy2015@gmail.com>
2025-07-28 10:56:47 +0100

    fix: delete bun.lockb file and fix lint

--- tests/utils/Assertions.sol
+++ tests/utils/Assertions.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// solhint-disable event-name-camelcase
+// solhint-disable event-name-capwords
 pragma solidity >=0.8.22;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
