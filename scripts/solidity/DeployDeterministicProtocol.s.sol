4af7b3afb599b88f439172debea13b07a64c8103
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-10-01 23:09:47 +0300

    feat: add nft descriptor addresses

--- scripts/solidity/DeployDeterministicProtocol.s.sol
+++ scripts/solidity/DeployDeterministicProtocol.s.sol
@@ -7,15 +7,28 @@ import { LockupNFTDescriptor } from "../../src/LockupNFTDescriptor.sol";
 import { SablierBatchLockup } from "../../src/SablierBatchLockup.sol";
 import { SablierLockup } from "../../src/SablierLockup.sol";
 
+import { LockupNFTDescriptorAddresses } from "./LockupNFTDescriptorAddresses.sol";
+
 /// @notice Deploys the Lockup Protocol at deterministic addresses across chains.
-contract DeployDeterministicProtocol is BaseScript {
+contract DeployDeterministicProtocol is BaseScript, LockupNFTDescriptorAddresses {
     /// @dev Deploys the protocol.
     function run()
         public
         returns (SablierLockup lockup, SablierBatchLockup batchLockup, LockupNFTDescriptor nftDescriptor)
     {
+        // If the contract is not already deployed, deploy it.
+        if (nftDescriptorAddress() == address(0)) {
+            // Use just the version as salt as we want to deploy at the same address across all chains.
+            bytes32 nftDescriptorSalt = bytes32(abi.encodePacked(getVersion()));
+
+            nftDescriptor = new LockupNFTDescriptor{ salt: nftDescriptorSalt }();
+        }
+        // Otherwise, use the address of the existing contract.
+        else {
+            nftDescriptor = LockupNFTDescriptor(nftDescriptorAddress());
+        }
+
         batchLockup = new SablierBatchLockup{ salt: SALT }();
-        nftDescriptor = new LockupNFTDescriptor{ salt: SALT }();
         lockup = new SablierLockup{ salt: SALT }(getComptroller(), address(nftDescriptor));
     }
 }
