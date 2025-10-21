882b0b1c0e9a8cc31c9f79cf5a8565c353b89080
Shubham Yadav <shubhamy2015@gmail.com>
2025-10-21 17:39:00 +0200

    refactor: chain id library and chainlink oracle fork tests (#67)
    * refactor: ChainId library
    test: refactor chainlink oracle fork test
    
    * ci: rename tree-path to test-dir
    
    * remove fuzzed paramter from chainlink oracle test
    
    * chore: polish wording in chain id
    
    chore: format foundry.toml
    
    * chore: set false "alignComments" for toml formatting
    
    ---------
    
    Co-authored-by: Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>

--- src/tests/ChainId.sol
+++ src/tests/ChainId.sol
@@ -1,29 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+// solhint-disable code-complexity
 pragma solidity >=0.8.22;
 
 library ChainId {
     /*//////////////////////////////////////////////////////////////////////////
-                                     FUNCTIONS
-    //////////////////////////////////////////////////////////////////////////*/
-
-    /// @notice Returns `true` if the given chain ID is supported.
-    function isSupported(uint256 chainId) internal pure returns (bool) {
-        bool isMainnet = chainId == ABSTRACT || chainId == ARBITRUM || chainId == AVALANCHE || chainId == BASE
-            || chainId == BERACHAIN || chainId == BLAST || chainId == BSC || chainId == CHILIZ || chainId == COREDAO
-            || chainId == ETHEREUM || chainId == GNOSIS || chainId == HYPEREVM || chainId == LIGHTLINK || chainId == LINEA
-            || chainId == MODE || chainId == MORPH || chainId == OPTIMISM || chainId == POLYGON || chainId == SCROLL
-            || chainId == SEI || chainId == SOPHON || chainId == SUPERSEED || chainId == SONIC || chainId == UNICHAIN
-            || chainId == XDC || chainId == ZKSYNC;
-
-        bool isTestnet = chainId == ARBITRUM_SEPOLIA || chainId == BASE_SEPOLIA || chainId == MODE_SEPOLIA
-            || chainId == OPTIMISM_SEPOLIA || chainId == SEPOLIA;
-
-        return isMainnet || isTestnet;
-    }
-
-    /*//////////////////////////////////////////////////////////////////////////
                                       MAINNETS
     //////////////////////////////////////////////////////////////////////////*/
+
+    uint256 public constant MAINNETS_COUNT = 26;
 
     uint256 public constant ABSTRACT = 2741;
     uint256 public constant ARBITRUM = 42_161;
@@ -48,7 +32,6 @@ library ChainId {
     uint256 public constant SONIC = 146;
     uint256 public constant SOPHON = 50_104;
     uint256 public constant SUPERSEED = 5330;
-    uint256 public constant TANGLE = 5845;
     uint256 public constant UNICHAIN = 130;
     uint256 public constant XDC = 50;
     uint256 public constant ZKSYNC = 324;
@@ -57,9 +40,113 @@ library ChainId {
                                       TESTNETS
     //////////////////////////////////////////////////////////////////////////*/
 
+    uint256 public constant TESTNETS_COUNT = 5;
+
     uint256 public constant ARBITRUM_SEPOLIA = 421_614;
     uint256 public constant BASE_SEPOLIA = 84_532;
     uint256 public constant MODE_SEPOLIA = 919;
     uint256 public constant OPTIMISM_SEPOLIA = 11_155_420;
     uint256 public constant SEPOLIA = 11_155_111;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                     FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Returns the full list of supported mainnet chain IDs.
+    function getAllMainnets() internal pure returns (uint256[] memory supportedIds) {
+        supportedIds = new uint256[](MAINNETS_COUNT);
+
+        supportedIds[0] = ABSTRACT;
+        supportedIds[1] = ARBITRUM;
+        supportedIds[2] = AVALANCHE;
+        supportedIds[3] = BASE;
+        supportedIds[4] = BERACHAIN;
+        supportedIds[5] = BLAST;
+        supportedIds[6] = BSC;
+        supportedIds[7] = CHILIZ;
+        supportedIds[8] = COREDAO;
+        supportedIds[9] = ETHEREUM;
+        supportedIds[10] = GNOSIS;
+        supportedIds[11] = HYPEREVM;
+        supportedIds[12] = LIGHTLINK;
+        supportedIds[13] = LINEA;
+        supportedIds[14] = MODE;
+        supportedIds[15] = MORPH;
+        supportedIds[16] = OPTIMISM;
+        supportedIds[17] = POLYGON;
+        supportedIds[18] = SCROLL;
+        supportedIds[19] = SEI;
+        supportedIds[20] = SOPHON;
+        supportedIds[21] = SUPERSEED;
+        supportedIds[22] = SONIC;
+        supportedIds[23] = UNICHAIN;
+        supportedIds[24] = XDC;
+        supportedIds[25] = ZKSYNC;
+    }
+
+    /// @notice Returns the full list of supported testnet chain IDs.
+    function getAllTestnets() internal pure returns (uint256[] memory supportedIds) {
+        supportedIds = new uint256[](TESTNETS_COUNT);
+
+        supportedIds[0] = ARBITRUM_SEPOLIA;
+        supportedIds[1] = BASE_SEPOLIA;
+        supportedIds[2] = MODE_SEPOLIA;
+        supportedIds[3] = OPTIMISM_SEPOLIA;
+        supportedIds[4] = SEPOLIA;
+    }
+
+    /// @notice Returns the chain name for the given chain ID.
+    function getName(uint256 chainId) internal pure returns (string memory chainName) {
+        // Mainnets.
+        if (chainId == ChainId.ABSTRACT) return "abstract";
+        if (chainId == ChainId.ARBITRUM) return "arbitrum";
+        if (chainId == ChainId.AVALANCHE) return "avalanche";
+        if (chainId == ChainId.BASE) return "base";
+        if (chainId == ChainId.BERACHAIN) return "berachain";
+        if (chainId == ChainId.BLAST) return "blast";
+        if (chainId == ChainId.BSC) return "bsc";
+        if (chainId == ChainId.CHILIZ) return "chiliz";
+        if (chainId == ChainId.COREDAO) return "core_dao";
+        if (chainId == ChainId.ETHEREUM) return "ethereum";
+        if (chainId == ChainId.GNOSIS) return "gnosis";
+        if (chainId == ChainId.HYPEREVM) return "hyperevm";
+        if (chainId == ChainId.LIGHTLINK) return "lightlink";
+        if (chainId == ChainId.LINEA) return "linea";
+        if (chainId == ChainId.MODE) return "mode";
+        if (chainId == ChainId.MORPH) return "morph";
+        if (chainId == ChainId.OPTIMISM) return "optimism";
+        if (chainId == ChainId.POLYGON) return "polygon";
+        if (chainId == ChainId.SCROLL) return "scroll";
+        if (chainId == ChainId.SEI) return "sei";
+        if (chainId == ChainId.SONIC) return "sonic";
+        if (chainId == ChainId.SOPHON) return "sophon";
+        if (chainId == ChainId.SUPERSEED) return "superseed";
+        if (chainId == ChainId.UNICHAIN) return "unichain";
+        if (chainId == ChainId.XDC) return "xdc";
+        if (chainId == ChainId.ZKSYNC) return "zksync";
+
+        // Testnets.
+        if (chainId == ChainId.ARBITRUM_SEPOLIA) return "arbitrum_sepolia";
+        if (chainId == ChainId.BASE_SEPOLIA) return "base_sepolia";
+        if (chainId == ChainId.MODE_SEPOLIA) return "mode_sepolia";
+        if (chainId == ChainId.OPTIMISM_SEPOLIA) return "optimism_sepolia";
+        if (chainId == ChainId.SEPOLIA) return "sepolia";
+    }
+
+    /// @notice Returns `true` if the given chain ID is supported.
+    function isSupported(uint256 chainId) internal pure returns (bool) {
+        // Return true if the chain ID is in the mainnet list.
+        uint256[] memory mainnets = getAllMainnets();
+        for (uint256 i = 0; i < mainnets.length; ++i) {
+            if (mainnets[i] == chainId) return true;
+        }
+
+        // Return true if the chain ID is in the testnet list.
+        uint256[] memory testnets = getAllTestnets();
+        for (uint256 i = 0; i < testnets.length; ++i) {
+            if (testnets[i] == chainId) return true;
+        }
+
+        return false;
+    }
 }
--- tests/fork/ChainlinkOracle.t.sol
+++ tests/fork/ChainlinkOracle.t.sol
@@ -2,55 +2,48 @@
 pragma solidity >=0.8.22 <0.9.0;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
 import { BaseScript } from "src/tests/BaseScript.sol";
-
+import { ChainId } from "src/tests/ChainId.sol";
 import { Base_Test } from "../Base.t.sol";
 
 contract BaseScriptMock is BaseScript { }
 
 contract ChainlinkOracle_Fork_Test is Base_Test {
-    /// @notice A modifier that runs the forked test for a given chain
-    modifier initForkTest(string memory chainName) {
-        // Fork chain on the latest block number.
-        vm.createSelectFork({ urlOrAlias: chainName });
+    function testFork_ChainlinkOracle() external {
+        // Get list of mainnet chain IDs.
+        uint256[] memory supportedChainIds = ChainId.getAllMainnets();
 
-        BaseScriptMock baseScriptMock = new BaseScriptMock();
+        // Run tests over all supported chains.
+        for (uint256 i = 0; i < 26; ++i) {
+            uint256 chainId = supportedChainIds[i];
 
-        // Get the Chainlink oracle address for the current chain.
-        address oracle = baseScriptMock.getChainlinkOracle();
+            // Skip zksync as it requires a different foundry version.
+            if (chainId == ChainId.ZKSYNC) continue;
 
-        // Retrieve the latest price and decimals from the Chainlink oracle.
-        (, int256 price,, uint256 updatedAt,) = AggregatorV3Interface(oracle).latestRoundData();
-        uint8 oracleDecimals = AggregatorV3Interface(oracle).decimals();
+            // Get the chain name.
+            string memory chainName = ChainId.getName(chainId);
 
-        // Assert that the Chainlink price feed returns non-zero values.
-        vm.assertGt(uint256(price), 0, "price");
-        vm.assertGt(updatedAt, 0, "updated at");
+            // Fork chain on the latest block number.
+            vm.createSelectFork({ urlOrAlias: chainName });
 
-        // Assert that the oracle returns 8 decimals.
-        vm.assertEq(oracleDecimals, 8, "oracle decimals");
+            BaseScriptMock baseScriptMock = new BaseScriptMock();
 
-        _;
+            // Get the Chainlink oracle address for the current chain.
+            address oracle = baseScriptMock.getChainlinkOracle();
+
+            // Skip if oracle is not found.
+            if (oracle == address(0)) continue;
+
+            // Retrieve the latest price and decimals from the Chainlink oracle.
+            (, int256 price,, uint256 updatedAt,) = AggregatorV3Interface(oracle).latestRoundData();
+            uint8 oracleDecimals = AggregatorV3Interface(oracle).decimals();
+
+            // Assert that the Chainlink price feed returns non-zero values.
+            vm.assertGt(uint256(price), 0, "price");
+            vm.assertGt(updatedAt, 0, "updated at");
+
+            // Assert that the oracle returns 8 decimals.
+            vm.assertEq(oracleDecimals, 8, "oracle decimals");
+        }
     }
-
-    function testFork_ChainlinkOracle_Arbitrum() external initForkTest("arbitrum") { }
-
-    function testFork_ChainlinkOracle_Avalanche() external initForkTest("avalanche") { }
-
-    function testFork_ChainlinkOracle_Base() external initForkTest("base") { }
-
-    function testFork_ChainlinkOracle_BSC() external initForkTest("bsc") { }
-
-    function testFork_ChainlinkOracle_Ethereum() external initForkTest("ethereum") { }
-
-    function testFork_ChainlinkOracle_Gnosis() external initForkTest("gnosis") { }
-
-    function testFork_ChainlinkOracle_Linea() external initForkTest("linea") { }
-
-    function testFork_ChainlinkOracle_Optimism() external initForkTest("optimism") { }
-
-    function testFork_ChainlinkOracle_Polygon() external initForkTest("polygon") { }
-
-    function testFork_ChainlinkOracle_Scroll() external initForkTest("scroll") { }
 }
