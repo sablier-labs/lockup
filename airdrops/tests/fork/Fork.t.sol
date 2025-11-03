7a37535ccfe3f25e102310b5f4c3b3a441c6115a
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-11-03 20:10:14 +0200

    ci: refactor CI workflows
    ci: create a common file
    
    temporarily don't run full-check job
    
    fix bulloak job
    
    use 1.4.4 forge version
    
    uncomment check job
    
    install deps in full-check
    
    ci: use working-directory
    
    ci: temp
    
    ci: refactor workflows
    
    ci: add names
    
    Signed-off-by: smol-ninja <shubhamy2015@gmail.com>
    
    add name
    
    identify which tests to run
    
    some more changes

--- airdrops/tests/fork/Fork.t.sol
+++ airdrops/tests/fork/Fork.t.sol
@@ -3,10 +3,6 @@ pragma solidity >=0.8.22 <0.9.0;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ISablierComptroller } from "@sablier/evm-utils/src/interfaces/ISablierComptroller.sol";
-import { ISablierFactoryMerkleInstant } from "src/interfaces/ISablierFactoryMerkleInstant.sol";
-import { ISablierFactoryMerkleLL } from "src/interfaces/ISablierFactoryMerkleLL.sol";
-import { ISablierFactoryMerkleLT } from "src/interfaces/ISablierFactoryMerkleLT.sol";
-import { ISablierFactoryMerkleVCA } from "src/interfaces/ISablierFactoryMerkleVCA.sol";
 import { ISablierLockup } from "@sablier/lockup/src/interfaces/ISablierLockup.sol";
 
 import { Base_Test } from "../Base.t.sol";
@@ -36,12 +32,9 @@ abstract contract Fork_Test is Base_Test {
         // Fork Ethereum Mainnet at the latest block number.
         vm.createSelectFork({ urlOrAlias: "ethereum" });
 
-        // Load deployed addresses from Ethereum Mainnet.
+        // TODO: Load deployed addresses from Ethereum Mainnet.
         comptroller = ISablierComptroller(0x0000008ABbFf7a84a2fE09f9A9b74D3BC2072399);
-        factoryMerkleInstant = ISablierFactoryMerkleInstant(0x7f70bA7C7373BaA4047cE450168cD24968321Bda);
-        factoryMerkleLL = ISablierFactoryMerkleLL(0x0781Ad660a5ED0041B45d44d45009a163CC0B578);
-        factoryMerkleLT = ISablierFactoryMerkleLT(0x336d464276e2c7C76927d975Ef866Df8a7Ecf8DD);
-        factoryMerkleVCA = ISablierFactoryMerkleVCA(0x91FdBd7077d615f951a0defA81Ec30Bfd68dbd8D);
+        deployFactoriesConditionally();
 
         // Update lockup address.
         lockup = ISablierLockup(0xcF8ce57fa442ba50aCbC57147a62aD03873FfA73);
