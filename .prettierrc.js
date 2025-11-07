6a4879bca7d1d92735ffe5e256733f1734f81db8
Andrei Vlad Birgaoanu <99738872+andreivladbrg@users.noreply.github.com>
2025-11-07 18:22:11 +0200

    ci: dry fy (#1305)
    * ci: refactor CI workflows
    
    ci: create a common file
    
    temporarily don't run full-check job
    
    fix bulloak job
    
    use 1.4.4 forge version
    
    uncomment check job
    
    install deps in full-check
    
    ci: use working-directory
    
    ci: temp
    
    ci: refactor workflows
    
    ci: add names
    
    Signed-off-by: smol-ninja <shubhamy2015@gmail.com>
    
    add name
    
    identify which tests to run
    
    some more changes
    
    * ci: pass meaningful names
    
    ---------
    
    Co-authored-by: smol-ninja <shubhamy2015@gmail.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,20 @@
-const baseConfig = require("@sablier/devkit/prettier");
+/**
+ * @notice Lint CIs run from within the protocol directories. So, prettier needs to resolve devkit from the protocol's
+ * node_modules folder.
+ */
+function resolveDevkit() {
+  try {
+    return require("@sablier/devkit/prettier");
+  } catch {
+    for (const pkg of ["airdrops", "flow", "lockup", "utils"]) {
+      try {
+        return require(`./${pkg}/node_modules/@sablier/devkit/.prettierrc.json`);
+      } catch {}
+    }
+  }
+}
+
+const baseConfig = resolveDevkit();
 
 /**
  * @see https://prettier.io/docs/configuration
