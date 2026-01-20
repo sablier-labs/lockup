602a86761af5dfd99274f20cb1212ab6a563be03
smol-ninja <shubhamy2015@gmail.com>
2026-01-20 17:21:53 +0000

    chore: remove unused imports

--- flow/tests/fork/Fork.t.sol
+++ flow/tests/fork/Fork.t.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.22;
 
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ISablierFlow } from "src/interfaces/ISablierFlow.sol";
 
 import { Base_Test } from "../Base.t.sol";
 
