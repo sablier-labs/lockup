042a52a65e710be72869c66d4cef65ca128a98c9
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2026-02-17 12:43:47 +0200

    feat: add _safeTokenSymbol function in bob

--- bob/src/SablierBob.sol
+++ bob/src/SablierBob.sol
@@ -109,7 +109,7 @@ contract SablierBob is
         }
 
         // Retrieve token symbol and token decimal.
-        string memory tokenSymbol = IERC20Metadata(address(token)).symbol();
+        string memory tokenSymbol = _safeTokenSymbol(address(token));
         uint8 tokenDecimals = IERC20Metadata(address(token)).decimals();
 
         // Effect: deploy the share token for this vault.
@@ -437,13 +437,61 @@ contract SablierBob is
     }
 
     /*//////////////////////////////////////////////////////////////////////////
-                         INTERNAL STATE-CHANGING FUNCTIONS
+                          PRIVATE STATE-CHANGING FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Checks whether the provided string contains only alphanumeric characters, spaces, and dashes.
+    /// @dev Note that this returns true for empty strings.
+    function _isAllowedCharacter(string memory str) private pure returns (bool) {
+        // Convert the string to bytes to iterate over its characters.
+        bytes memory b = bytes(str);
+
+        uint256 length = b.length;
+        for (uint256 i = 0; i < length; ++i) {
+            bytes1 char = b[i];
+
+            // Check if it's a space, dash, or an alphanumeric character.
+            bool isSpace = char == 0x20; // space
+            bool isDash = char == 0x2D; // dash
+            bool isDigit = char >= 0x30 && char <= 0x39; // 0-9
+            bool isUppercaseLetter = char >= 0x41 && char <= 0x5A; // A-Z
+            bool isLowercaseLetter = char >= 0x61 && char <= 0x7A; // a-z
+            if (!(isSpace || isDash || isDigit || isUppercaseLetter || isLowercaseLetter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// @notice Retrieves the token's symbol safely, defaulting to a hard-coded value if an error occurs.
+    /// @dev Performs a low-level call to handle tokens in which the symbol is not implemented or it is a bytes32
+    /// instead of a string.
+    function _safeTokenSymbol(address token) private view returns (string memory) {
+        (bool success, bytes memory returnData) = token.staticcall(abi.encodeCall(IERC20Metadata.symbol, ()));
+
+        // Non-empty strings have a length greater than 64, and bytes32 has length 32.
+        if (!success || returnData.length <= 64) {
+            return "ERC20";
+        }
+
+        string memory symbol = abi.decode(returnData, (string));
+
+        // Check if the symbol is too long or contains disallowed characters. This measure helps mitigate potential
+        // security threats from malicious tokens injecting scripts in the symbol string.
+        if (bytes(symbol).length > 30) {
+            return "Long Symbol";
+        } else {
+            if (!isAllowedCharacter(symbol)) {
+                return "Unsupported Symbol";
+            }
+            return symbol;
+        }
+    }
 
     /// @dev Internal function to fetch the latest oracle price and update it in the vault storage.
     /// @param vaultId The ID of the vault.
     /// @return latestPrice The latest price from the oracle.
-    function _syncPriceFromOracle(uint256 vaultId) internal returns (uint128 latestPrice) {
+    function _syncPriceFromOracle(uint256 vaultId) private returns (uint128 latestPrice) {
         AggregatorV3Interface oracleAddress = _vaults[vaultId].oracle;
 
         // Get the latest price from the oracle with safety checks.
@@ -462,7 +510,7 @@ contract SablierBob is
     /// @dev Internal function to unstake all tokens using the adapter.
     /// @param vaultId The ID of the vault.
     /// @return amountReceivedFromAdapter The amount of tokens received from the adapter after unstaking.
-    function _unstakeFullAmountViaAdapter(uint256 vaultId) internal returns (uint128 amountReceivedFromAdapter) {
+    function _unstakeFullAmountViaAdapter(uint256 vaultId) private returns (uint128 amountReceivedFromAdapter) {
         Bob.Vault storage vault = _vaults[vaultId];
 
         // Get the total amount staked via the adapter.
